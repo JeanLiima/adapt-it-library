cc5c67891e278f992fe6534c55123cc65ec14ffa
Jean Lima <jean.car.liima@gmail.com>
2023-02-26 20:00:54 -0300

    Format after git commit

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,20 +1,17 @@
-import { InputHTMLAttributes } from "react";
+import React from "react";
+import { type InputHTMLAttributes } from "react";
 
-import Styles from "./styles.module.scss"
+import Styles from "./styles.module.scss";
 
-export interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+export interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string
 }
 
-function TextField(props: Props) {
-  return (
-    <div
-      className={Styles.text}
-    >
+const TextField: React.FC<TextFieldProps> = (props) => (
+    <div className={Styles.text}>
       <span>{props.label}</span>
       <input {...props} />
     </div>
-  );
-}
+);
 
 export default TextField;
